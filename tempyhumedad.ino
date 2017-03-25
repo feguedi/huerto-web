5e16e8b35c5174a560f597aac40a2526f00ae99a
armandohdz0623 <armandohdz06@gmail.com>
2017-03-24 23:21:02 -0600

    Add files via upload

---
+++ tempyhumedad.ino
@@ -0,0 +1,33 @@
+const int sensorPin= A0;
+int Valor;  
+
+ 
+void setup()
+{
+  Serial.begin(9600);
+}
+ 
+void loop()
+{
+  int value = analogRead(sensorPin);
+  float millivolts = (value / 1023.0) * 5000;
+  float celsius = millivolts / 100; 
+  Serial.print(celsius);
+  Serial.println(" C");
+  delay(1000);
+ Serial.print("Sensor de Humedad valor:");  
+Valor = analogRead(1);  
+Serial.print(Valor);  
+if (Valor <= 300)  
+   Serial.println(" Encharcado");  
+if ((Valor > 300) and (Valor <= 700))  
+    Serial.println(" Humedo, no regar");   
+if (Valor > 700)  
+    Serial.println(" Seco, necesitas regar");  
+delay(1000);
+}
+
+        
+      
+
+
